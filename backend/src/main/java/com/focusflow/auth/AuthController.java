a1b67ae783e60a1399dc762c598fc7b3eb67bf5a
SandyaVutla <vutlasandhya84@gmail.com>
2026-02-22 14:46:33 +0530

    authcontroller changes

--- backend/src/main/java/com/focusflow/auth/AuthController.java
+++ backend/src/main/java/com/focusflow/auth/AuthController.java
@@ -2,13 +2,18 @@ package com.focusflow.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
@@ -20,9 +25,15 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signUpRequest) {
-        System.out.println(">>> SIGNUP HIT");
-        System.out.println(signUpRequest.getEmail());
-        return ResponseEntity.ok("SIGNUP CONTROLLER WORKS");
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest) {
+
+        User user = new User();
+        user.setName(signupRequest.getName());
+        user.setEmail(signupRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+
+        System.out.println(">>> USER CREATED WITH ENCODED PASSWORD: " + user.getEmail());
+
+        return ResponseEntity.ok("USER OBJECT OK");
     }
 }
